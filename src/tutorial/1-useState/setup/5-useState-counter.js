6845bde66a6c202e3070a3603f5842f8afe38f60
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-05-03 18:51:34 -0400

    setTimeout and prevState

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,14 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // gets the current value right before the update
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
 
   return (
     <>
@@ -19,6 +27,14 @@ const UseStateCounter = () => {
         </button>
         <button className='btn' onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+      {/* this counter uses timeout */}
+      <section style={{ margin: '4rem 0' }}>
+        <h2>Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
