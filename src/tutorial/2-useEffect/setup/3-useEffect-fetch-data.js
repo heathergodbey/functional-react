6ba6d87107b4819448fdd521893adb50b7449f5d
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-05-04 19:42:18 -0400

    useEffect fetch data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,39 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getusers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    //console.log(users);
+  };
+
+  useEffect(() => {
+    // perfrom fetch request
+    getusers();
+  }, []); // dependency array required due to triggering rendering
+
+  return (
+    <>
+      <h3>GitHub Users</h3>
+      <uL className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}> Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </uL>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
