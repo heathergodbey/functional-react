665cd8ea5c76932f8034809efea5785b1fc4277f
Heather Godbey <51833484+heathergodbey@users.noreply.github.com>
2021-05-08 14:24:30 -0400

    short circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,19 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>Value: {secondValue}</h1> */}
+      <h1>{text || 'john doe'}</h1>
+      {text && <h1>hello world</h1>}
+      {!text && <h1>hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
